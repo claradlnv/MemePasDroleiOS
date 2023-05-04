92d793ebb1d1a33e557d3f94312714b3e3a7bfe7
Clara <delianov.clara@gmail.com>
2023-05-04 13:06:09 +0200

    Initial Commit

---
+++ Meme Pas Drole/Meme_Pas_DroleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meme_Pas_DroleApp.swift
+//  Meme Pas Drole
+//
+//  Created by Clara Delianov on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Meme_Pas_DroleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
